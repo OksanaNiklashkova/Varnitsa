2c3c5cfe26e156b660c524c356f291e7bec2f36a
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-10-07 14:15:44 +0300

    create templates home and gate-age pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from users.mixins import AgeVerificationRequiredMixin
+
+
+class HomeView(AgeVerificationRequiredMixin, TemplateView):
+    template_name = 'catalog/home.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['featured_beers'] = Beer.objects.filter(is_published=True)[:3]
+    #     return context
--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,9 @@ from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
-    path('users/', include('users.urls', namespace='users')),
+    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('', include('users.urls', namespace='users')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
